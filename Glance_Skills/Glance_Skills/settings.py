c90c8dc86a0527404c33e8031d5b6282e8502867
anujsajwan9 <anusajwan0808@gmail.com>
2022-07-20 23:59:21 +0530

    Database

--- Glance_Skills/Glance_Skills/settings.py
+++ Glance_Skills/Glance_Skills/settings.py
@@ -76,16 +76,23 @@ WSGI_APPLICATION = 'Glance_Skills.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'Glance',
+#         'USER':'postgres',
+#         'PASSWORD':'1234',
+#         'HOST':'localhost'
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Glance',
-        'USER':'postgres',
-        'PASSWORD':'1234',
-        'HOST':'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
